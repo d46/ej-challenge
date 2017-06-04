ad2f0a90d5519817463b51bc483a1890c8e57a4e
daghangunay <daghangunay@gmail.com>
2017-06-05 02:17:39 +0300

    change package name

--- ej-service/src/main/java/com/ejchallange/badge/service/filter/CORSFilter.java
+++ ej-service/src/main/java/com/ejchallange/badge/service/filter/CORSFilter.java
@@ -1,4 +1,4 @@
-package com.ejchallange.badge.service.filters;
+package com.ejchallange.badge.service.filter;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
